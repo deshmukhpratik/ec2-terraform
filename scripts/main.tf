bb6f6c6ebf8482e4e59d84d8c8fdfe62742b846b
deshmukhpratik <97725286+deshmukhpratik@users.noreply.github.com>
2023-05-04 10:16:33 +0530

    Create main.tf

---
+++ scripts/main.tf
@@ -0,0 +1,95 @@
+1.A Variable block where we define all the resource names that we are going to be using within the Terraform configuration.
+2.The second block is to tell Terraform to choose the right provider, in our case it is aws and we are also defining the region 
+in this block on which our resources should be created.
+3.Creating a Security Group with inbound and outbound rules. We have two inbound rules and one outbound rule. 
+we use lifecycle block to tell terraform to create the replacement resources first before destroying the live ones. this way we reduce downtime
+4.Creating an EC2 instance, The instance type would be picked up from the variables block and we give some meaningful tags for management and future identification.
+5.Once the EC2 instance created, we would get the public IP of the instance. We are saving it as an output variable. 
+The output variables would be saved locally and can be viewed anytime in the future with terraform output command
+
+
+
+
+variable "awsprops" {
+    type = "map"
+    default = {
+    region = "us-east-1"
+    vpc = "vpc-5234832d"
+    ami = "ami-0c1bea58988a989155"
+    itype = "t2.micro"
+    subnet = "subnet-81896c8e"
+    publicip = true
+    keyname = "myseckey"
+    secgroupname = "IAC-Sec-Group"
+  }
+}
+
+provider "aws" {
+  region = lookup(var.awsprops, "region")
+}
+
+resource "aws_security_group" "project-iac-sg" {
+  name = lookup(var.awsprops, "secgroupname")
+  description = lookup(var.awsprops, "secgroupname")
+  vpc_id = lookup(var.awsprops, "vpc")
+
+  // To Allow SSH Transport
+  ingress {
+    from_port = 22
+    protocol = "tcp"
+    to_port = 22
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  // To Allow Port 80 Transport
+  ingress {
+    from_port = 80
+    protocol = ""
+    to_port = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+
+resource "aws_instance" "project-iac" {
+  ami = lookup(var.awsprops, "ami")
+  instance_type = lookup(var.awsprops, "itype")
+  subnet_id = lookup(var.awsprops, "subnet") #FFXsubnet2
+  associate_public_ip_address = lookup(var.awsprops, "publicip")
+  key_name = lookup(var.awsprops, "keyname")
+
+
+  vpc_security_group_ids = [
+    aws_security_group.project-iac-sg.id
+  ]
+  root_block_device {
+    delete_on_termination = true
+    iops = 150
+    volume_size = 50
+    volume_type = "gp2"
+  }
+  tags = {
+    Name ="SERVER01"
+    Environment = "DEV"
+    OS = "UBUNTU"
+    Managed = "IAC"
+  }
+
+  depends_on = [ aws_security_group.project-iac-sg ]
+}
+
+
+output "ec2instance" {
+  value = aws_instance.project-iac.public_ip
+}
